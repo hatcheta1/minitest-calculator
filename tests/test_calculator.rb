ba038a39f84daee468d77ea5c4851afe10ae4674
Ashanti Hatchett <ahatchett02@gmail.com>
2024-07-02 22:33:17 +0000

    wrote test for add function in Calculator class

--- tests/test_calculator.rb
+++ tests/test_calculator.rb
@@ -1,6 +1,10 @@
 
-class Calculator
-  def add(a,b)
-    a + b
+require 'minitest/autorun'
+require './calculator'
+
+class TestCalculator < Minitest::Test
+  def test_addition
+    calculator = Calculator.new
+    assert_equal 4, calculator.add(2, 2), "Addition method failed"
   end
 end
